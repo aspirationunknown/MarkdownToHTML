c6ab4250ce2ab4b9a6aa5491e29abd336e7c6f6b
Charlie Parsons <prisonofidentity@gmail.com>
2026-06-29 07:45:08 -0400

    Refactored to have file resource acquired in constructor and closed in destructor

--- inc/markdownconverter.h
+++ inc/markdownconverter.h
@@ -1,15 +1,17 @@
+#include <fstream>
 #include <string>
 
 enum class InputFileStatus { Ok = 0, Failed_To_Open };
 
 class MarkdownConverter {
 public:
-    MarkdownConverter() : input_file_name_("") {}
-    MarkdownConverter(std::string input_file_name) : input_file_name_(input_file_name) {}
-    ~MarkdownConverter() {}
+    MarkdownConverter();
+    MarkdownConverter(std::string input_file_name);
+    ~MarkdownConverter();
 
     const std::string GetInputFileName() { return input_file_name_; }
     InputFileStatus ReadInputFile();
 private:
-    std::string input_file_name_;
+    std::string input_file_name_{};
+    std::ifstream input_file_;
 };
--- src/markdownconverter.cc
+++ src/markdownconverter.cc
@@ -1,17 +1,28 @@
-#include <fstream>
 #include <iostream>
 
 #include "markdownconverter.h"
 
+MarkdownConverter::MarkdownConverter() {
+    input_file_.open(input_file_name_, std::ios::in);
+}
+
+MarkdownConverter::MarkdownConverter(std::string input_file_name) : input_file_name_(input_file_name) {
+    MarkdownConverter();
+}
+
+MarkdownConverter::~MarkdownConverter() {
+    input_file_.close();
+}
+
 InputFileStatus MarkdownConverter::ReadInputFile() {
     auto result{InputFileStatus::Ok};
-    std::ifstream input_file;
-    input_file.open(input_file_name_, std::ios::in);
-    if (!input_file) {
+
+    input_file_.open(input_file_name_, std::ios::in);
+    if (!input_file_) {
         result = InputFileStatus::Failed_To_Open; // Failure to open input file
     } else {
         std::string line{};
-        while (getline(input_file, line)) {
+        while (getline(input_file_, line)) {
             std::cout << line << std::endl;
         }
     }
