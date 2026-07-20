5216f57828f42aa1020af5cfabce32a5d1c5d9f6
Charlie Parsons <prisonofidentity@gmail.com>
2026-07-20 02:40:36 -0400

    Prevented copying and moving of MarkdownConverter

--- inc/markdownconverter.h
+++ inc/markdownconverter.h
@@ -5,6 +5,10 @@ public:
     MarkdownConverter() = delete;
     MarkdownConverter(std::string input_file_name);
     ~MarkdownConverter();
+    MarkdownConverter(const MarkdownConverter&) = delete;
+    MarkdownConverter(MarkdownConverter&&) = delete;
+    MarkdownConverter& operator=(const MarkdownConverter&) = delete;
+    MarkdownConverter&& operator=(MarkdownConverter&&) = delete;
 
     InputFileStatus ReadInputFile() override;
 };
