c18f2e3e3200da5fd13a438dac41540b796f7858
Charlie Parsons <prisonofidentity@gmail.com>
2026-08-15 04:53:41 -0400

    Refactor variable name and remove unnecessary include

--- test/tokenqueue_test.cc
+++ test/tokenqueue_test.cc
@@ -1,16 +1,15 @@
 #include <gtest/gtest.h>
-#include <memory>
 
 #include "node.h"
 #include "tokenqueue.h"
 
 TEST(TokenQueueTest, PushTest) {
     TokenQueue token_queue{};
-    auto empty_bold_text_node = std::make_unique<BoldTextNode>("Some words");
+    auto bold_text_node = std::make_unique<BoldTextNode>("Some words");
 
     ASSERT_EQ(token_queue.Size(), 0);
 
-    token_queue.Push(std::move(empty_bold_text_node));
+    token_queue.Push(std::move(bold_text_node));
 
     ASSERT_EQ(token_queue.Size(), 1);
 }
